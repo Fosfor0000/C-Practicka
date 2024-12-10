288dd52ba93c1ead6c926c9e615411f221a2193b
Віталій <vitaliishm09@gmail.com>
2024-12-10 19:58:17 +0200

    Pr1

---
+++ Practic1/Pr1.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practika_1
+{
+    
+    public class Practic1
+    {
+        static void Main()
+        {
+
+            List<Task> tasks = new List<Task>
+        {
+            new Task1(),
+            new Task2(),
+            new Task3(),
+            new Task4()
+        };
+
+
+            foreach (var task in tasks)
+            {
+                task.Execute();
+            }
+        }
+
+    }
+        
+    
+    
+}
